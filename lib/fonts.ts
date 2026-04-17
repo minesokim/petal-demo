ca27348966c3b723c9a08c099623f9bb1d594d46
David Kim <davidkim@Davids-MacBook-Air-3.local>
2026-04-17 02:58:09 -0700

    Strip unused fonts, fix Vercel build failures.

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,149 +1,21 @@
-import {
-  Geist,
-  Inter,
-  Montserrat,
-  Overpass_Mono,
-  Poppins,
-  Roboto,
-  PT_Sans,
-  Plus_Jakarta_Sans,
-  Hedvig_Letters_Serif,
-  Kumbh_Sans,
-  Outfit,
-  DM_Sans,
-  Lato,
-  Sora,
-  Manrope,
-  Cormorant,
-  Libre_Franklin,
-  IBM_Plex_Sans,
-  Albert_Sans
-} from "next/font/google";
+import { Geist, DM_Sans, Geist_Mono } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-
 const geist = Geist({ subsets: ["latin"], variable: "--font-geist" });
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-roboto"
-});
-
-const plus_jakarta_sans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "800"],
-  variable: "--font-plus-jakarta-sans"
-});
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
-  variable: "--font-montserrat"
-});
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
-  variable: "--font-poppins"
-});
-
-const overpass_mono = Overpass_Mono({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-overpass-mono"
-});
-
-const ptSans = PT_Sans({
-  variable: "--font-pt-sans",
-  subsets: ["latin"],
-  weight: ["400", "700"]
-});
-
-const hedvig_letters_serif = Hedvig_Letters_Serif({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-hedvig-letters-serif"
-});
-
-const kumbh_sans = Kumbh_Sans({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-kumbh-sans"
-});
-
-const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-outfit"
-});
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
   variable: "--font-dm-sans"
 });
 
-const cormorant = Cormorant({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-cormorant"
-});
-
-const libreFranklin = Libre_Franklin({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-  variable: "--font-libre-franklin"
-});
-
-const albertSans = Albert_Sans({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-  variable: "--font-albert-sans"
-});
-
-const ibmPlexSans = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-  variable: "--font-ibm-plex-sans"
-});
-
-const manrope = Manrope({
+const geistMono = Geist_Mono({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
-  variable: "--font-manrope"
-});
-
-const sora = Sora({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
-  variable: "--font-sora"
-});
-
-const lato = Lato({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-  variable: "--font-lato"
+  variable: "--font-geist-mono"
 });
 
 export const fontVariables = cn(
   geist.variable,
-  inter.variable,
-  roboto.variable,
-  montserrat.variable,
-  poppins.variable,
-  overpass_mono.variable,
-  ptSans.variable,
-  plus_jakarta_sans.variable,
-  hedvig_letters_serif.variable,
-  kumbh_sans.variable,
-  outfit.variable,
   dmSans.variable,
-  sora.variable,
-  manrope.variable,
-  cormorant.variable,
-  libreFranklin.variable,
-  ibmPlexSans.variable,
-  albertSans.variable,
-  lato.variable
+  geistMono.variable
 );
